3bc2e94a40b0bd8457905ed88ea267e404a78b57
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-10-09 01:09:09 +0530

    some

--- brokart/orders/urls.py
+++ brokart/orders/urls.py
@@ -14,16 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-    path('customer/',include('customers.urls')),
-    path('orders/',include('orders.urls'))
+    path('cart', views.show_cart,name='cart'),
+    
     
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
