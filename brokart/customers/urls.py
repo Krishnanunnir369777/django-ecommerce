ffb4d8cbbafcefb1f5ac7c2742df459a9b307671
Krishnanunnir369777 <krishnanunnir503@gmail.com>
2025-12-04 23:38:15 +0530

    some

--- brokart/customers/urls.py
+++ brokart/customers/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from . import views
 urlpatterns = [
     path('account', views.show_account,name='account'),
-    
+    path('logout', views.sign_out,name='logout'),
     
 ]
 
